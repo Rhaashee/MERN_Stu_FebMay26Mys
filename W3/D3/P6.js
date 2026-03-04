f2e99c633f6b12dfc35f095d240ef554351de88c
Rhaashee <rhaaasheel@gamil.com>
2026-03-04 14:30:15 +0530

    Nested Objects

---
+++ W3/D3/P6.js
@@ -0,0 +1,18 @@
+// Nested Objects
+const student = {
+    firstName:"Rhaashee",
+    lastName:"Lokesh",
+    scores:{
+        Maths : 80,
+        Science : 90
+    },
+    hobbies:["Reading","Dancing"],
+    fullname: function(){
+        return this.firstName + " "+this.lastName;
+    },
+    greet(){
+        console.log("Hi" ,this.fullname());
+    },
+};
+console.log(student.fullname());
+student.greet();
