d3b61dc830fbd07b5838dbb4cbd88a19a6e89a96
Rhaashee <rhaaasheel@gamil.com>
2026-03-15 12:19:51 +0530

    W4-portfolio mini project

---
+++ W4/D5/portfolio-project/js/features/project-search.js
@@ -0,0 +1,55 @@
+function searchProject(){
+    const projectsContainer = document.getElementById("projects-container");
+    if(!projectsContainer){
+        console.log("Project container not found");
+        return;
+    }
+    const projectSearch = document.createElement("project-search");
+    if(!projectSearch){
+        console.log("Project Search not found");
+        return;
+    }
+    projectsContainer.innerHTML="";
+
+    projectsData.forEach(function(project){
+        //to create an outer card
+        const card=document.createElement("div");
+        card.className="p-8 text-center bg-white rounded-3xl shadow-lg";
+
+        //create icon
+        const iconBox=document.createElement("div");
+        iconBox.className="w-20 h-20 mx-auto mb-4 bg-green-900 rounded-2xl flex item-center justify-center ";
+
+        //create icon text
+        const iconText=document.createElement("span");
+        iconText.className="text-2xl text-white font-bold";
+        iconText.textContent=project.shortLabel;
+
+        //puts icontext inside iconbox
+        iconBox.appendChild(iconText);
+
+        //create the project name
+        const projectName=document.createElement("h2");
+        projectName.className="text-xl font-bold mb-2";
+        projectName.textContent=project.name;
+
+        //create the project ID
+        const projectId=document.createElement("h2");
+        projectId.className="text-xl font-bold mb-2";
+        projectId.textContent=project.id;
+
+        //create project description
+        const projectDescription=document.createElement("p");
+        projectDescription.className="text-sm";
+        projectDescription.textContent=project.description;
+
+        //appending all child elements to card
+        card.appendChild(iconBox);
+        card.appendChild(projectId);
+        card.appendChild(projectName);
+        card.appendChild(projectDescription);
+        //append card to projects container
+        projectsContainer.appendChild(card);
+    });
+    console.log("projects container successfully");
+}
