4d6eb6672f0531bb0be85658f04e80d8d107de69
Rhaashee <rhaaasheel@gamil.com>
2026-03-26 16:25:20 +0530

    Sending  responce in JSON format

---
+++ W6/D4/P6.js
@@ -0,0 +1,23 @@
+// Sending  responce in JSON format
+const http = require("http");
+
+const server = http.createServer(function (req, res) {
+    if (req.url === "/api/status" && req.method === "GET") {
+        const responseData = {
+            success: true,
+            message: "API is working",
+            server: "NodeJS HTTP Module"
+        };
+        // JSON response using application/json
+        res.writeHead(200, { "Content-Type": "application/json" });
+        // JSON.stringfy() converts JS object into a JSON string
+        res.end(JSON.stringify(responseData));
+        return;
+    }
+    res.writeHead(404,{"Content-Type":"application/json"});
+    res.end(JSON.stringify({success:false,message:"Route not found."}));
+});
+
+server.listen(3004,function(){
+    console.log("Server is running at http://localhost:3004");
+});
