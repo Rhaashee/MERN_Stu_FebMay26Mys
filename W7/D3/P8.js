4a777b48cd85012744c054d0f8dcd4f7f68b7030
Rhaashee <rhaaasheel@gamil.com>
2026-04-02 12:45:53 +0530

    JWT flow with login, refresh-style login ans secure verification

--- W7/D3/P8.js
+++ W7/D3/P8.js
@@ -1,110 +1,116 @@
-// JWT flow with login ,refresh-style logic as well as secure verification 
-const express = require("express");
-const jwt = require("jsonwebtoken");
+//JWT flow with login, refresh-style login ans secure verification
 
-const app = express(); 
+const jwt = require("jsonwebtoken");
+const express = require("express");
+
+const app = express();
 app.use(express.json());
 
-const secretKey = "MysecretKey";
-const refreshsecretKey = "MyNewsecretKey";
+const secretKey = "Mysecretekey";
+const refreshSecretKey = "MyNewsecretekey";
 
-// Indicates IN-MEMORY STORAGE for refresh tokens 
+//in memeory storage for refresh token
 const refreshTokens = [];
-
-function authenticateAccessToken(req, res, next) {
-    const authHeader = req.headers.authorization;
-    const token = authHeader && aunthHeader.split(" ")[1];
+//authenticate access token
+function authentiacateAccessToken(req,res,next){
+    const authHeader = req.headers.authorisation;
+    
+    const token = authHeader && authHeader.split(" ")[1];
     if (!token) {
         return res.status(401).json({
             success: false,
             message: "Bearer token is missing."
         });
     }
-    try {
-        req.user = jwt.verify(token, secretKey, {
-            algorithms :["HS256"],
-            issuer : "jwt-example"
+    try{
+        req.user = jwt.verify(token,secretKey,{
+            algorithms:["HS256"],
+            issuer:"jwt-example"
         });
         next();
-    } catch (error) {
+    }
+    catch(error){
         if(error.name === "TokenExpiredError"){
             return res.status(401).json({
-                success : false,
-                message : "Access token has expired"
+                success: false,
+                message: "Access token has expired."
             });
         }
         return res.status(401).json({
-            success : false,
-            message : "Access token is invalid"
+            success: false,
+            message: "Access token is invalid."
         });
     }
 }
 
+//Post function used login
 app.post("/login",function(req,res){
-    const{email,password} = req.body;
-    if(email !== "email@email.com" || password !== "pass@123"){
-        return response.staus(401).json({
-            success : false,
-            message :"Invalid Credentials"
+    const {email,password} = req.body;
+    if(email!=="email@email.com" || password!=="pass@123"){
+        return res.status(401).json({
+            success:false,
+            message:"Invalid Credentials"
         });
     }
     const accessToken = jwt.sign({
-        userId : 101,
-        email : email,
-        role : "member"
-    },secretKey,{expiresIn:"10m",
-        algorithm : "HS256", 
-        issuer : "jwt-example"});
+        userId:101,
+        email:email,
+        role:"member"
+    },secretKey,{expiresIn:"10m",algorithm:"HS256",issuer:"jwt-example"});
 
-    // Resigning / refreshimg tokens 
     const refreshToken = jwt.sign({
-        userId : 101,
-        email : email
-        
-    },refreshsecretKey,{expiresIn:"10d" //d:days m:mins h:hour 
-        ,algorithm : "HS256", 
-        issuer : "jwt-example"});
-        refreshTokens.push(refreshToken);
-        res.json({
-            success:true,
-            message: "Login successful",
-            accessToken: accessToken,
-            refreshToken:refreshToken
-        });
+        userId:101,
+        email:email
+    },refreshSecretKey,
+    {
+        expiresIn:"10d",//d:days m:min h:hour
+        algorithm:"HS256",
+        issuer:"jwt-example"
+    });
+    refreshTokens.push(refreshToken);
+    res.json({
+        success:true,
+        message:"login successful",
+        accessToken:accessToken,
+        refreshToken:refreshToken
+    });
 });
 
 app.post("/refresh",function(req,res){
-    const{refreshToken} = req.body;
-    if (!refreshToken || !refreshTokens.includes(refreshToken)) {
+    const {refreshToken} = req.body;
+    if(!refreshToken || !refreshTokens.includes(refreshToken)){
         return res.status(401).json({
             success:false,
-            message:"Refresh token is missing or invalid."
+            message:"Refresh token is missing or invalid"
         });
     }
-    try {
-        const decoded = jwt.verify(refreshToken,refreshsecretKey,{
-        algorithms : ["HS256"], 
-        issuer : "jwt-example"
+    try{
+        const decoded = jwt.verify(refreshToken,refreshSecretKey,{
+            algorithms:["HS256"],
+            issuer:"jwt-example"
         });
         const newAccessToken = jwt.sign({
             userId:decoded.userId,
             email:decoded.email,
             role:"member"
-        },secretKey,{expiresIn:"10m",
-        algorithms : "HS256", 
-        issuer : "jwt-example"});
-        res.join({
+        },secretKey,{
+            expiresIn:"10m",//d:days m:min h:hour
+            algorithm:"HS256",
+            issuer:"jwt-example"
+        });
+        res.json({
             success:true,
             accessToken:newAccessToken
         });
-    } catch (error) {
+    }
+    catch(error){
         res.status(403).json({
             success:false,
-            message:"Refresh token Verification failed."
+            message:"Refresh token verification failed."
         });
     }
 });
-app.get("/me",authenticateAccessToken,function(req,res){
+app.get("/me",authentiacateAccessToken,function(req,res){
     res.json({
         success:true,
         message:"Protected user route",
@@ -113,9 +119,8 @@ app.get("/me",authenticateAccessToken,function(req,res){
 });
 
 app.listen(4000,function(){
-    console.log("JWT Demo server running @ http://localhost:4000");
-}); 
-
+    console.log("JWT demo server running @ http://localhost:4000");
+});
 // curl -X POST http://localhost:4000/login -H "Content-Type: application/json" -d "{\"email\":\"email@email.com\",\"password\":\"pass@123\"}"
-// curl -X POST http://localhost:4000/refresh -H "Content-Type: application/json" -d "{\"email\":\"email@email.com\",\"password\":\"pass@123\"}"
-// curl -X POST http://localhost:4000/me -H "Content-Type: application/json" -d "{\"email\":\"email@email.com\",\"password\":\"pass@123\"}"
+// curl -X POST http://localhost:4000/refresh -H "Content-Type: application/json" -d "{\"refreshToken\":\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEwMSwiZW1haWwiOiJlbWFpbEBlbWFpbC5jb20iLCJpYXQiOjE3NzUxMTA5NzUsImV4cCI6MTc3NTk3NDk3NSwiaXNzIjoiand0LWV4YW1wbGUifQ.3dVf91zzjiebRSih-luME9HKgzfqsyCi5oC_7cktiRE\"}"
+// curl http://localhost:4000/me -H "Authorisation:Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEwMSwiZW1haWwiOiJlbWFpbEBlbWFpbC5jb20iLCJyb2xlIjoibWVtYmVyIiwiaWF0IjoxNzc1MTEzODEzLCJleHAiOjE3NzUxMTQ0MTMsImlzcyI6Imp3dC1leGFtcGxlIn0.8KG0MdP5uMcvzz1gAuPsSBhcIe4D_YPX_-xgd7wkcRY"
