aa4d58bee2d1c23e8f325bb1a8e3ee64f45688ac
Rhaashee <rhaaasheel@gamil.com>
2026-04-14 12:31:31 +0530

    Add createPost function to create and store user posts

---
+++ Projects/Guided Project/LinkedInClone/posts.js
@@ -0,0 +1,28 @@
+// Add createPost function to create and store user posts
+const store = require("./data");
+const eventEmitter = require("./events");
+
+async function createPost(content) {
+    try {
+        if (!store.currentUser) throw "Login required";
+
+        const post = {
+            id: Date.now(),
+            authorId: store.currentUser.id,
+            content,
+            timestamp: new Date(),
+            likes: [],
+            comments: []
+        };
+
+        store.posts.push(post);
+
+        eventEmitter.emit("postCreated");
+        return post;
+
+    } catch (error) {
+        eventEmitter.emit("operationFailed", error);
+    }
+}
+
+module.exports = { createPost };
