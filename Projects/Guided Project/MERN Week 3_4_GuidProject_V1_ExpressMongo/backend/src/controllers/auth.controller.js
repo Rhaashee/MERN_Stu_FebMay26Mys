a45bcfb4c073b6e149d7a706da14e0754f0d082e
Rhaashee <rhaaasheel@gamil.com>
2026-04-21 15:24:14 +0530

    Controller function created wrt auth

--- Projects/Guided Project/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/controllers/auth.controller.js
+++ Projects/Guided Project/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/controllers/auth.controller.js
@@ -1,45 +1,48 @@
 const authService = require("../services/auth.service");
 
-// Register
+//Register
 exports.register = async(req,res,next)=>{
-    try {
+    try{
         const result = await authService.registerUser(req.body);
 
         res.status(201).json({
             success:true,
-            message:"User registered.OTP sent.",
+            message:"User registed. OTP sent.",
             data:result,
         });
-    } catch (error) {
-        next (error);
+    }
+    catch(error){
+        next(error);
     }
 };
 
-// Verify OTP
+//Veriy otp
 exports.verifyOTP = async(req,res,next)=>{
-    try {
+    try{
         await authService.verifyOTP(req.body);
 
         res.status(200).json({
             success:true,
-            message:"OTP Verified successfully",
+            message:"OTP verified successfully",
         });
-    } catch (error) {
-        next(error)
+    }
+    catch(error){
+        next(error);
     }
 };
 
-// Login 
-exports.login = async(req,res,next)=>{
-    try {
+//Login
+exports.login = async(req,res,next) => {
+    try{
         const result = await authService.loginUser(req.body);
 
-        res.status(200).json({success:true,
-            message:"Login successful",
-            data:result
+        res.status(200).json({
+            success:true,
+            message:"Login Successful",
+            data:result,
         });
-            
-    } catch (error) {
+    }
+    catch(error){
         next(error);
     }
-}
+};
