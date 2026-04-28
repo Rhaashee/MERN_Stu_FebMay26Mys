29d90401579e1462fa001f6f19b7960ae4f0ea29
Rhaashee <rhaaasheel@gamil.com>
2026-04-28 12:29:21 +0530

    Created Simple Functional Component

---
+++ W9/D2/my-app/src/components/FunctionalComponentsBasics.jsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+function Welcome(props){
+    // Child Component : ruable UI
+    return(
+        <p>Hello, {props.name}</p>
+    );
+}
+
+// Parent class 
+export function FunctionalComponentsBasics(){
+    return(
+        <div>
+            <h2>Functional Components Basics</h2>
+            {/* We are passing 'Rhaashee' as prop and Welcome function receives is as {name : "Rhaashee"}*/}
+            <Welcome name = "Rhaashee"/>
+
+            {/* We are passing 'Developer' as prop and Welcome function receives is as {name : "Developer"}*/}
+            <Welcome name = "Developer"/>
+        </div>
+    )
+}
