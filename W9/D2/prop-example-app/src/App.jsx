c3b7dd6162c2e33519ac79c8272f08cf02e09b1a
Rhaashee <rhaaasheel@gamil.com>
2026-04-29 09:58:46 +0530

    Updated PropChildren Component

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,14 +1,16 @@
 
 import './App.css'
 // import { PropBasics } from "./components/P1"
-import { PropDestructuring } from "./components/P2"
+// import { PropDestructuring } from "./components/P2"
+import { PropsChildren } from './components/P3';
 
 function App() {
 
   return (
     <>
       {/* <PropBasics/> */}
-      <PropDestructuring/>
+      {/* <PropDestructuring/> */}
+      <PropsChildren />
     </>
   )
 }
