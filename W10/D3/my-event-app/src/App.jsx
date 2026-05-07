1bb8fb7c841df3b5df480294f4c4a4dbe790e677
Rhaashee <rhaaasheel@gamil.com>
2026-05-07 15:41:05 +0530

    Stop Event Propagation

--- W10/D3/my-event-app/src/App.jsx
+++ W10/D3/my-event-app/src/App.jsx
@@ -5,7 +5,8 @@ import './App.css'
 // import { UseCallbackEvents } from "./components/P5";
 // import { EventDelegation } from "./components/P6";
 // import { FormsPattern } from "./components/P7";
-import { KeyboardEvent } from "./components/P8";
+// import { KeyboardEvent } from "./components/P8";
+import { EventObj } from "./components/P9";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
       {/* <UseCallbackEvents /> */}
       {/* <EventDelegation /> */}
       {/* <FormsPattern /> */}
-      <KeyboardEvent />
+      {/* <KeyboardEvent /> */}
+      <EventObj />
     </>
   )
 }
