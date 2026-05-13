ebf2005a34310ebd7da2c10b6ba8ab9b030c4d7b
Rhaashee <rhaaasheel@gamil.com>
2026-05-13 12:43:42 +0530

    Optional parameters

---
+++ W11/D2/my-app/src/components/P4.jsx
@@ -0,0 +1,13 @@
+// Optional parameters 
+import { useParams } from "react-router-dom";
+
+export function OptionalParam() {
+    const { username }=useParams();
+
+    return(
+        <div>
+            <h2>Optional Parameters</h2>
+            {username ? (<p>Hello, {username}</p>):( <p>Hello,User</p>)}
+        </div>
+    ) 
+}  
